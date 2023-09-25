4dea72cfef7331bf04d6f57f215123b19fc909cb
coldestEight <113954719+coldestEight@users.noreply.github.com>
2023-09-25 13:44:52 -0400

    commented gptResponses.py

--- gptResponses.py
+++ gptResponses.py
@@ -5,12 +5,16 @@ import dotenv
 dotenv.load_dotenv()
 openai.api_key = env["OPENAI_API_KEY"]
 
+#Makes call to GPT API and formats response
 def getMsgResponse(messageLog):
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = messageLog, temperature=1,max_tokens=300)
     rawOut = response.choices[0].message.content
 
     punctuationList = [".","?","!"]
     cutoffPos = 0
+
+    #Check for last punctuation point and split the string at that point
+    #Prevents incomplete sentences from being sent back to AI
     for i in range(len(rawOut)):
         if rawOut[i] in punctuationList:
             cutoffPos = i
